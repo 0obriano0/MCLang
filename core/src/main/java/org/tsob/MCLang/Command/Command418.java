35134e65cc88f8956fb91eb10cbc1cbc816278c4
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2025-05-29 13:04:56 +0800

    調整 根據各個版本 預先將各版本語言包放進去

---
+++ core/src/main/java/org/tsob/MCLang/Command/Command418.java
@@ -0,0 +1,27 @@
+package org.tsob.MCLang.Command;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Command418 extends mainCommandSystem{
+
+  public Command418() {
+    super(  "418",
+        "/MCLang 418 你可以試試看這個指令",
+        new ArrayList<String>(Arrays.asList("mclang.user.418")));
+  }
+  
+  @Override
+  public void run(CommandSender sender, String commandLabel, Command command, String[] args) throws Exception {
+    sender.sendMessage("I'm a clay pot");
+  }
+  
+  @Override
+  public void run(Player player, String commandLabel, Command command, String[] args) throws Exception {
+    run((CommandSender)player, commandLabel, command,args);
+  }
+}
